42d131feac6e7030c8866e5da6e454565651f50c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-05-02 21:11:06 -0300

    Create JvmParamsCompareApplication.java

---
+++ JVMParamsCompare/src/main/java/tech/noetzold/JVMParamsCompare/JvmParamsCompareApplication.java
@@ -0,0 +1,13 @@
+package tech.noetzold.JVMParamsCompare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvmParamsCompareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvmParamsCompareApplication.class, args);
+	}
+
+}
