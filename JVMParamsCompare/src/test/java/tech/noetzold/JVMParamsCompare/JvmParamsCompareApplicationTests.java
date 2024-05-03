340ec6652ef99a4face97f62d951782c2b47e3eb
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-05-02 21:11:12 -0300

    Create JvmParamsCompareApplicationTests.java

---
+++ JVMParamsCompare/src/test/java/tech/noetzold/JVMParamsCompare/JvmParamsCompareApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.noetzold.JVMParamsCompare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvmParamsCompareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
