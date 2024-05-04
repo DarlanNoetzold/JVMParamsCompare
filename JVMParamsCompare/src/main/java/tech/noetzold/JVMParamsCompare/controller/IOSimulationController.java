f609bcfc1344ffa30b69a730b582936dba7fc705
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-05-04 13:55:28 -0300

    Update IOSimulationController.java

--- JVMParamsCompare/src/main/java/tech/noetzold/JVMParamsCompare/controller/IOSimulationController.java
+++ JVMParamsCompare/src/main/java/tech/noetzold/JVMParamsCompare/controller/IOSimulationController.java
@@ -1,16 +1,24 @@
 package tech.noetzold.JVMParamsCompare.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.noetzold.JVMParamsCompare.model.IODataModel;
+import tech.noetzold.JVMParamsCompare.service.IODataService;
+
 import java.io.*;
 
 @RestController
 @RequestMapping("/io-simulation")
 public class IOSimulationController {
 
+    @Autowired
+    private IODataService ioDataService;
+
     @GetMapping("/simulate")
-    public String simulateIOOperations() throws IOException {
+    public ResponseEntity<IODataModel> simulateIOOperations() throws IOException {
         File tempFile = File.createTempFile("test", ".txt");
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile))) {
             for (int i = 0; i < 10000; i++) {
@@ -18,7 +26,6 @@ public class IOSimulationController {
             }
         }
 
-        // Lendo o arquivo
         StringBuilder output = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(tempFile))) {
             String line;
@@ -27,9 +34,10 @@ public class IOSimulationController {
             }
         }
 
-        // Deletando o arquivo temporário
         tempFile.delete();
+        IODataModel ioDataModel = ioDataService.saveData(output.toString());
 
-        return "Operações de I/O simuladas com sucesso.";
+
+        return ResponseEntity.ok(ioDataModel);
     }
 }
