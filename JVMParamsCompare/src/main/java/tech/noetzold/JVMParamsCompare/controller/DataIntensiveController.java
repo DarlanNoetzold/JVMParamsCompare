950a93c0987bc37b3bce690632bcf66d81ec3cac
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-05-11 08:17:40 -0300

    Update DataIntensiveController.java

--- JVMParamsCompare/src/main/java/tech/noetzold/JVMParamsCompare/controller/DataIntensiveController.java
+++ JVMParamsCompare/src/main/java/tech/noetzold/JVMParamsCompare/controller/DataIntensiveController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.noetzold.JVMParamsCompare.message.config.RabbitmqQueues;
 import tech.noetzold.JVMParamsCompare.model.DataIntensiveModel;
 import tech.noetzold.JVMParamsCompare.service.DataIntensiveService;
+import tech.noetzold.JVMParamsCompare.service.RabbitmqService;
 
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
@@ -19,15 +21,14 @@ public class DataIntensiveController {
     @Autowired
     DataIntensiveService dataIntensiveService;
 
+    @Autowired
+    private RabbitmqService rabbitmqService;
+
     @GetMapping("/process/{size}")
     public ResponseEntity<DataIntensiveModel> processDataIntensively(@PathVariable Long size) {
-        String result = LongStream.rangeClosed(1, size)
-                .mapToObj(Long::toString)
-                .collect(Collectors.joining(", "));
+        rabbitmqService.sendMessage(RabbitmqQueues.DATA_INTESIVE_QUEUE, new DataIntensiveModel(0L, size, ""));
 
-        DataIntensiveModel dataIntensiveModel = dataIntensiveService.saveResult(new DataIntensiveModel(0L, size, result));
-
-        return ResponseEntity.ok(dataIntensiveModel);
+        return ResponseEntity.ok(new DataIntensiveModel(0L, size, ""));
     }
 }
 
