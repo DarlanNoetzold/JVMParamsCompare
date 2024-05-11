fd9de94e9b8905777233352341db8340231f8126
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-05-11 08:18:09 -0300

    Update DataIntensiveService.java

--- JVMParamsCompare/src/main/java/tech/noetzold/JVMParamsCompare/service/DataIntensiveService.java
+++ JVMParamsCompare/src/main/java/tech/noetzold/JVMParamsCompare/service/DataIntensiveService.java
@@ -4,8 +4,10 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tech.noetzold.JVMParamsCompare.model.DataIntensiveModel;
-import tech.noetzold.JVMParamsCompare.model.HeavyCalculationModel;
 import tech.noetzold.JVMParamsCompare.repository.DataIntensiveRepository;
+
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 
 @Service
 public class DataIntensiveService {
@@ -15,6 +17,13 @@ public class DataIntensiveService {
 
     @Transactional
     public DataIntensiveModel saveResult(DataIntensiveModel dataIntensiveModel){
+
+        String result = LongStream.rangeClosed(1, dataIntensiveModel.getSize())
+                .mapToObj(Long::toString)
+                .collect(Collectors.joining(", "));
+
+        dataIntensiveModel.setResult(result);
+
         return dataIntensiveRepository.save(dataIntensiveModel);
     }
 }
