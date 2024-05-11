bd579df2f3a9b4123533febc77a1b98d9239d62d
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-05-11 08:17:55 -0300

    Create RabbitmqQueues.java

---
+++ JVMParamsCompare/src/main/java/tech/noetzold/JVMParamsCompare/message/config/RabbitmqQueues.java
@@ -0,0 +1,10 @@
+package tech.noetzold.JVMParamsCompare.message.config;
+
+public class RabbitmqQueues {
+
+    public static final String DATA_QUEUE = "DATA";
+    public static final String DATA_INTESIVE_QUEUE = "DATA_INTESIVE";
+    public static final String HEAVY_CALCULATION_QUEUE = "HEAVY_CALCULATION";
+    public static final String IO_SIMULATION_QUEUE = "IO_SIMULATION";
+
+}
