1def224bf3b8927b7aa515349311479be5b8ef4a
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-06-02 10:00:11 -0300

    Delete HeavyCalculationRepository.java

--- JVMParamsCompare/src/main/java/tech/noetzold/JVMParamsCompare/repository/HeavyCalculationRepository.java
+++
@@ -1,9 +0,0 @@
-package tech.noetzold.JVMParamsCompare.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tech.noetzold.JVMParamsCompare.model.HeavyCalculationModel;
-
-@Repository
-public interface HeavyCalculationRepository extends JpaRepository<HeavyCalculationModel, Long> {
-}
