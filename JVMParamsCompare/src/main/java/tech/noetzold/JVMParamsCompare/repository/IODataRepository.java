f38911edcedcb704ba2f728c106766c921fd28bb
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-06-02 10:00:10 -0300

    Delete IODataRepository.java

--- JVMParamsCompare/src/main/java/tech/noetzold/JVMParamsCompare/repository/IODataRepository.java
+++
@@ -1,9 +0,0 @@
-package tech.noetzold.JVMParamsCompare.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tech.noetzold.JVMParamsCompare.model.IODataModel;
-
-@Repository
-public interface IODataRepository extends JpaRepository<IODataModel, Long> {
-}
