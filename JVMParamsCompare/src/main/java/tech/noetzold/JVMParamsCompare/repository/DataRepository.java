a772431bc31bf36ceff76a92c04f51b4a159e019
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-06-02 10:00:14 -0300

    Delete DataRepository.java

--- JVMParamsCompare/src/main/java/tech/noetzold/JVMParamsCompare/repository/DataRepository.java
+++
@@ -1,9 +0,0 @@
-package tech.noetzold.JVMParamsCompare.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tech.noetzold.JVMParamsCompare.model.DataModel;
-
-@Repository
-public interface DataRepository extends JpaRepository<DataModel, Long> {
-}
